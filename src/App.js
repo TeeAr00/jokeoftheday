b8f3b986924e6e31d4dab4050e21397dfc50536c
TeeAr00 <t1rate00@student.oamk.fi>
2022-10-17 11:53:44 +0300

    axios kirjaston käyttö

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+
+import { useState,useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  const [title, setTitle] = useState('');
+  const [text, setText] = useState('');
+
+const URL= 'https://api.jokes.one/jod'
+
+  useEffect(() => {
+    axios.get(URL)
+    .then((response) =>{
+    const joke = response.data.contents.jokes[0].joke
+    setTitle(joke.title)
+    setText(joke.text)
+    }).catch(error =>{
+      console.log(error)
+      alert("Retrieving joke of the day failed!")
+    })
+  }, [])
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Joke of the day</h3>
+      <h4>{title}</h4>
+      <p>{text}</p>
     </div>
   );
 }
